9e5b81ab0c868740faec8b4bd3e33ef9f878cab8
Daniel Banegas <dbanegasl@gmail.com>
2023-10-24 11:24:47 -0500

    changes

--- version.php
+++ version.php
@@ -2,8 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023101810;        // Versión actual del plugin (Fecha: AñoMesDiaXX).
+$plugin->version   = 2023102400;        // Versión actual del plugin (Fecha: AñoMesDiaXX).
 $plugin->requires  = 2020061500;        // Versión mínima requerida de Moodle (3.8).
 $plugin->component = 'local_sgevea';    // Nombre completo del plugin.
 $plugin->maturity = 'MATURITY_STABLE';  // Estabilidad de Codigo
-$plugin->release = '1.1';
+$plugin->release = '1.2';
